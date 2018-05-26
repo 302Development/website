0b97993b573612a82e94bc6e8f98dfa4efde8adb
jay-donovan <36951183+jay-donovan@users.noreply.github.com>
2018-05-26 20:09:50 +0800

    Ajax Update
    updated to make more stable and now supports all tabs but performace

--- assets/js/pullData.js
+++ assets/js/pullData.js
@@ -1,55 +1,40 @@
 
-//var timestamp = new Date();
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-let DashboardData;
-if (this.readyState == 4 && this.status == 200) {
-	//alert (this.responseText);
-	test = JSON.parse(this.responseText);
-	//alert(test["devices"]["0"]["type"]);
-	count = 0;
-        
+		var timestamp = new Date();
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+            let DashboardData;
+            if (this.readyState == 4 && this.status == 200) {
+                //alert (this.responseText);
+				test = JSON.parse(this.responseText);
+				//alert(test["devices"]["0"]["type"]);
+				count = 0;
+                count2 = 0;
 
-	for ( count in test["devices"]){
-	
-	if ((test["devices"][count]["type"]) === "network") {
-		//alert("test");
-		var inMbs = parseFloat(test["devices"][count]["Mb/sIn"]);
-		var inMbsID = (test["devices"][count]["Mb/sInID"]);
-		var outMbs = parseFloat(test["devices"][count]["Mb/sOut"]);
-		var outMbsID = (test["devices"][count]["Mb/sOutID"]);
-		//alert(inMbsID);
-		var gaugeElement = document.getElementById(('Canvas') + (inMbsID));
-		gaugeElement.setAttribute('data-value', inMbs);
-		gaugeElement.setAttribute('data-title' , "Download");
-	
-		var gaugeElement = document.getElementById(('Canvas') + (outMbsID));
-		gaugeElement.setAttribute('data-value', outMbs);
-		gaugeElement.setAttribute('data-title' , "Upload");
-        //animateGauges(inMbs);
-        
-	//alert (inMbs);
-		}
-	else if ((test["devices"][count]["type"]) === "performance"){
-		
-		
-		//alert("test");
-		
-	}
+				for ( count in test["devices"]){
+				if ((test["devices"][count]["type"]) === "network") {
+						//alert("test");
+						var inMbs = parseFloat(test["devices"][count]["Mb/sIn"]);
+						var outMbs = parseFloat(test["devices"][count]["Mb/sOut"]);
+
+                    	var gaugeElement = document.getElementsByTagName('canvas')[count2];
+                    	gaugeElement.setAttribute('data-value', inMbs);
+                    	count2 = count2 + 1
+                    	var gaugeElement = document.getElementsByTagName('canvas')[count2];
+                    	gaugeElement.setAttribute('data-value', outMbs);
+                    	gaugeElement.setAttribute('data-title' , "Upload");
+                    	//animateGauges(inMbs);
+                    	count2 = count2 + 1
+						//alert (inMbs);
+					}
 
 
-	count = count + 1;
-		}
+				count = count + 1;
+				}
 
 
             }
 		};
-		function RequestJSON(){
-		var timestamp = new Date();
 		xhttp.open("GET", ("dashboard.json" + "?" + timestamp.getTime()), true);
 		xhttp.send();
-		setInterval(RequestJSON, 60000);
-		}
-RequestJSON();
 
 

fcf457a5aba2c421fe2972d2174a7bdca967bc02
jay-donovan <36951183+jay-donovan@users.noreply.github.com>
2018-05-30 20:39:52 +0800

    update

---
+++ assets/js/StandardPres.js
@@ -0,0 +1,13 @@
+for (var count = 50; count > -1; count--){
+searchFor = "TileNo" + count;
+var tile = document.getElementById(searchFor);
+if (tile !==null) {
+
+var isVis = tile.getAttribute("presVisible");
+var isDis = tile.getAttribute("Visible");
+
+if (isDis === "1"){
+    tile.style.display = "none";
+}
+}
+}
